812763150e97523b6743f6c9409446415e0ed50b
dlinch <dlinch33@gmail.com>
2016-03-03 12:18:56 -0700

    complete rehaul inspired by drew dahlman

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+  console.log("Hello there. Welcome to my humble abode.")
+  $('.bookToRead').on('mouseover', function(){
+    $('.construction').removeClass('hidden');
+  })
+})
