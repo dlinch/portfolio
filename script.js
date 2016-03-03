04c21574991805d7b3cf8f1d695394d46f47b4c1
dlinch <dlinch33@gmail.com>
2016-03-03 13:14:29 -0700

    added jquery to toggle under construction for booktoread, added books list with current and upcoming books I'm reading

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
   console.log("Hello there. Welcome to my humble abode.")
-  $('.bookToRead').on('mouseover', function(){
+  $('.bookToRead').on('mouseenter', function(){
     $('.construction').removeClass('hidden');
   })
+
+  $('.bookToRead').on('mouseleave', function(){
+    $('.construction').addClass('hidden');
+  })
+
+  
+
 })
