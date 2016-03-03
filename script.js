2f44978080c82f0a706408b561bb7fc40101a16e
dlinch <dlinch33@gmail.com>
2016-03-03 14:01:28 -0700

    added tech to each project, hated it so I took it back out.

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
-
   console.log("Hello there. Welcome to my humble abode.")
   $('.bookToRead').on('mouseenter', function(){
     $('.construction').fadeIn("slow");
   })
-
   $('.bookToRead').on('mouseleave', function(){
     $('.construction').fadeOut("slow");
   })
-
-
-
 })
