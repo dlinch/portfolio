8d191ac503e616f4bc7e3500f574ca058a4a7882
dlinch <dlinch33@gmail.com>
2016-03-03 13:20:07 -0700

    added fading to construction hiding/displaying

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
   console.log("Hello there. Welcome to my humble abode.")
   $('.bookToRead').on('mouseenter', function(){
-    $('.construction').removeClass('hidden');
+    $('.construction').fadeIn("slow");
   })
 
   $('.bookToRead').on('mouseleave', function(){
-    $('.construction').addClass('hidden');
+    $('.construction').fadeOut("slow");
   })
 
-  
+
 
 })
